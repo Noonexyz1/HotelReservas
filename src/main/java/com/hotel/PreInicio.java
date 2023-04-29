cc9f87b80d0a1a1f19eb93c3147c7b2b3cda535c
Noonexyz1 <aries-d2github@hotmail.com>
2023-04-29 15:30:50 -0400

    implementacion de WebSocket con Servlets

--- src/main/java/com/hotel/PreInicio.java
+++ src/main/java/com/hotel/PreInicio.java
@@ -1,21 +1,63 @@
 package com.hotel;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
 @WebServlet(name = "PreInicio", urlPatterns = {"/"})
+@ServerEndpoint(value = "/updates")
 public class PreInicio extends HttpServlet {
-
+    
+    private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<>());
+    //private static final Set<Session> sessions = Collections.synchronizedSet(Set.of());
+    
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
         //redirigimos a la ventana de bienvenida
-        response.sendRedirect("bienvenida.jsp");
-        
+        request.getRequestDispatcher("bienvenida.jsp").forward(request, response);
     }
+    
+    
+    
+    @OnOpen
+    public void onOpen(Session session) throws IOException {
+        sessions.add(session);
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) throws IOException {
+        for (Session s : sessions) {
+            if (s.isOpen()) {
+                s.getBasicRemote().sendText(message);
+            }
+        }
+    }
+
+    @OnClose
+    public void onClose(Session session) throws IOException {
+        sessions.remove(session);
+    }
+
+    @OnError
+    public void onError(Throwable throwable) {
+        // Do error handling here
+    }
+    
+    
+    
 
 }
--- src/main/webapp/bienvenida.jsp
+++ src/main/webapp/bienvenida.jsp
@@ -3,12 +3,31 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>WebSocket Example</title>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script>
+                $(document).ready(function () {
+                var socket = new WebSocket("ws://localhost:8080/HotelReservas/updates");
+                socket.onmessage = function (event) {
+                    var message = event.data;
+                    $('#messages').append('<p>' + message + '</p>');
+                }
+                $('#sendButton').click(function () {
+                    var message = $('#message').val();
+                    socket.send(message);
+                });
+            });
+        </script>
     </head>
     
     <body>
         
-        <h1>Hola jsp</h1>
+        <h1>WebSocket Example</h1>
+        
+        <div id="messages"></div>
+        
+        <input type="text" id="message">
+        <button id="sendButton">Send</button>
         
     </body>
     
